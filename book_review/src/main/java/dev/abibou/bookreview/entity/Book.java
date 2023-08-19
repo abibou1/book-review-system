1c59b1dae17b8629b9a6859a19952a1249ae83fc
abibou1 <abibou2017@gmail.com>
2023-08-19 05:07:43 -0400

    added book entity

--- book_review/src/main/java/dev/abibou/bookreview/entity/Book.java
+++ book_review/src/main/java/dev/abibou/bookreview/entity/Book.java
@@ -1,5 +1,30 @@
 package dev.abibou.bookreview.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name="book")// optional as table and entity have the exact name
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Data
 public class Book {
+	@Id
+	@Column(name="book_id")
+	private int id;
+	@Column
+	private String author;
+	@Column
+	private String title;
+	@Column
+	private String publisher;
+	
+	
+	
 
 }
