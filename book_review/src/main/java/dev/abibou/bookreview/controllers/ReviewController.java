5a02d8ce75987d90a45b3f797d2f8bb7cff8de40
abibou1 <abibou2017@gmail.com>
2023-09-15 06:15:53 -0400

    created get-reviews-of book_id

--- book_review/src/main/java/dev/abibou/bookreview/controllers/ReviewController.java
+++ book_review/src/main/java/dev/abibou/bookreview/controllers/ReviewController.java
@@ -1,11 +1,15 @@
 package dev.abibou.bookreview.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.abibou.bookreview.entity.Review;
@@ -15,6 +19,7 @@ import dev.abibou.bookreview.utils.JwtUtil;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
+@RequestMapping("/authenticated/review")
 public class ReviewController {
 	@Autowired
 	ReviewService reviewService;
@@ -22,7 +27,7 @@ public class ReviewController {
 	@Autowired
 	JwtUtil jwtUtil;
 	
-	@PostMapping("/authenticated/review/write/{book_id}")
+	@PostMapping("/write/{book_id}")
 	public ResponseEntity<?> writeReview(@PathVariable ("book_id") int bookId,
 			@RequestHeader("Authorization") String headerToken,
 			@RequestBody String comment){
@@ -41,6 +46,13 @@ public class ReviewController {
 		
 		return new ResponseEntity<>(review, HttpStatus.CREATED);
 		
+	}
+	
+	@GetMapping("/get-reviews-of/{book_id}")
+	public ResponseEntity<?> getReviewsOf(@PathVariable ("book_id") int bookId){
+		List<Review> reviewsOf = reviewService.getReviewsByBook_id(bookId);
+		
+		return new ResponseEntity<>(reviewsOf, HttpStatus.OK);
 	}
 
 }
