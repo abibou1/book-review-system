c2c7981ef2528cc05909a5332241aae5712b988f
abibou1 <abibou2017@gmail.com>
2025-05-01 04:14:42 -0400

    added @activeProfiles on tests

--- book_review/src/test/java/dev/abibou/bookreview/BookReviewApplicationTests.java
+++ book_review/src/test/java/dev/abibou/bookreview/BookReviewApplicationTests.java
@@ -3,6 +3,7 @@ package dev.abibou.bookreview;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class BookReviewApplicationTests {
 
